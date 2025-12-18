d880487699fa3ab8fe6cfa0b5127d327aa72a11a
CunhazadaNoDale <cunha12324@gmail.com>
2025-12-17 22:55:09 -0300

    style: formatação corrigida

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { vitePluginManusRuntime } from "vite-plugin-manus-runtime";
 
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime()];
 
+
 export default defineConfig({
   plugins,
   resolve: {
